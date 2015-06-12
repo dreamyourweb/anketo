bc6b060ad1baf162b62ddf3d91fa1414a130309f
Hanneke van den Akker <hanneke@orikami.nl>
2015-06-12 15:21:10 +0200

    message after filling out form

--- client/views/formQuin.js
+++ client/views/formQuin.js
@@ -13,9 +13,9 @@ Template.formQuin.rendered = function(){
 	});
 };
 
-Template.formQuin.events({
-    'submit form': function(){
-    	console.log('test');
+Template.formQuin.events({	
+    'submit form': function(e){
+    	e.preventDefault();
     	emailAddress = document.getElementById('email').value;
 
     	doc = {Voornaam: document.getElementsByName('Voornaam')[0].value, 
@@ -23,7 +23,13 @@ Template.formQuin.events({
     		Bedrijf: document.getElementsByName('Bedrijf')[0].value};
 
 
-    	Meteor.call('sendEmail', emailAddress, doc, function(err, result) {		   
+    	Meteor.call('sendEmail', emailAddress, doc, function(err, result) {	
+			if (err) {
+    			console.log(err);
+			}
+			else
+			{FlashMessages.sendSuccess("Bedankt voor het achterlaten van uw gegevens. Wij nemen binnenkort contact met u op.");}
 		 });
      }
 });
+
